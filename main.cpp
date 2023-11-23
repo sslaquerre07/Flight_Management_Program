10333b9355fcb5b50afedc452b36536ecc2b007d
sslaquerre07 <125930722+sslaquerre07@users.noreply.github.com>
2023-11-23 16:23:16 -0700

    Menu display adde d and implemented

--- main.cpp
+++ main.cpp
@@ -6,12 +6,15 @@
 #include "header.h"
 using namespace std;
 
+void print_title();
+void menu_display();
+
 int main(){
     print_title();
     //Read text file provided with info and assign accordingly
     int option = 0;
     while(1){
-        //Menu display function
+        menu_display();
         cin >> option;
         //Check if the option is a valid integer here.
         if(option == 1){
@@ -30,6 +33,7 @@ int main(){
             //Save Data Function
         }
         else if(option == 6){
+            cout << "Goodbye";
             break;
         }
         else{
@@ -46,4 +50,14 @@ void print_title(){
     cout << "Term Project - Flight Management Program in C++" << endl;
     cout << "Produced By: Sam Laquerre, Amielle El Makhzoumi" << endl;
     cout << "Year: 2023" << endl;
+}
+
+void menu_display(){
+    cout << "Please select one of the following options:" << endl;
+    cout << "1. Display Flight Seat Map" << endl;
+    cout << "2. Display Passenger Information" << endl;
+    cout << "3. Add a New Passenger" << endl;
+    cout << "4. Remove an Existing Passenger" << endl;
+    cout << "5. Save data" << endl;
+    cout << "6. Quit" << endl;
 }
